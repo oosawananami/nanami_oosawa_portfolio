4d0f98f120c1120161d4def0355ec1e81efcb7b1
nanami <h_daniel_min@yahoo.co.jp>
2022-06-05 18:59:47 +0900

    js

---
+++ script.js
@@ -0,0 +1,30 @@
+//上に戻るボタンstart
+$(function() {
+  var showflag = false;
+  var pagetop = $('#page_top');
+  pagetop.css('bottom', '-100px');
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {  //100pxスクロールしたら
+      if (showflag == false) {
+        showflag = true;
+        pagetop.stop().animate({
+          'bottom': '50px' //下から50pxの位置に
+        }, 300); //0.3秒かけて現れる
+      }
+    } else {
+      if (showflag) {
+        showflag= false;
+        pagetop.stop().animate({
+          'bottom': '-50px' //下から-50pxの位置に
+        }, 300); //0.3秒かけて隠れる
+      }
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500); //0.5秒かけてトップへ戻る
+    return false;
+  });
+  //上に戻るボタンend
+
+});
